bc47e8fae4130e1bef2f51373c85dd7715aab8f4
Lauren Mak <laurenmak93@gmail.com>
2018-04-04 11:44:51 -0600

    CHANGELOG: Scripts, PHX, auxiliary programs.
    Orig2Recons: Got rid of position-by-position closest-reconstruction-to-original comparison, and replaced it by all-reconstructions-to-original differences. Implemented scraping of interpool and intrapool reconstructed and original haplotype frequencies, and output of difference between reconstructed - original with a flexible similarity threshold. Will report extended results to a text file (breakdowns of RH-OH accuracies, inter- and intrapool frequency comparisons) and summary statistics to STDOUT for both GC and entire-PHX results.
    
    HaploWriter and PatientSim: Have merged these functionalities into FullSimulator in conjunction with the use of the program ms. ms simulates 0/1 haplotypes based on user-specified initial conditions (ex. mutation rate, number of generations, etc.) which allows for the simulation of 1) shared AND independent mutations and 2) linkage. Results not shown but reconstruction is markedly improved when there is actual linkage to detect. Also, the number of haplotypes a patient gets is equal to the number of haplotypes being simulated, so that each haplotype can hypothetically occur once in a patient. Though the identity of each assigned haplotype is still random, there will theoretically be the same amount of simulated haplotype across all patients. Can consider re-introducing randomizing the number of haplotypes per patient (to simulate different viral load conditions) in the future. Can consider switching read simulator to ART now, since the mutation function of dwgsim is no longer needed and ART seems more like the field standard. 
    
    PoolHapX: Fixed intrapool counts to intrapool frequencies. For some reason, it still 'breaks' sometimes, possibly due to computational difficulties of arithmetic involving numbers close to 0. Added a catch statement for the graph-colouring algorithm. If graph-colouring a particular pool is unsatisfiable, then the program will keep running.
    
    Script: Have added the full simulated-sequence pipeline, Pipeline_RandSim_v0.2.pl, and the accompanying parameter text file, parameters_v0.2. Script should be completely automated i.e.: will generate everything from the reference and parameter files through haplotypes without any manual input. Have also written an automator script (and corresponding version of the parameter file) that runs through different values for i) the population mutation rate and ii) the number of haplotypes simulated (the OHs and randomized haplotypes per patient).

---
+++ src/MiscFunctions/FullSimulator.java
@@ -0,0 +1,142 @@
+package MiscFunctions;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.ThreadLocalRandom;
+import org.apache.commons.lang3.ArrayUtils;
+
+public class FullSimulator {
+	
+	// args[0] is the reference sequence.
+	// args[1] is the variant composition of the simulated haplotypes.
+	// args[2] is the rate of indels given the rate of mutations.
+	// args[3] is the probability that an insertion is extended.
+	// args[4] is the output directory.
+	// args[5] is the number of patients.
+	// args[6] is the number of haplotypes assigned to each patient.
+
+	public static void main(String[] args) throws IOException {
+		
+		// 1) HashMap<totPos,ref> and ArrayList<availPos>. 
+		// Put all reference bases in an indexed object.
+		BufferedReader br1 = new BufferedReader(new FileReader(args[0])); 
+		HashMap<Integer,String> refSequence = new HashMap<Integer,String>();
+		String currLine = br1.readLine();
+		int tmpPos = 0; 
+		while (currLine != null) {
+			if (currLine.contains(">")) {
+				currLine = br1.readLine();
+				continue; 
+			}
+			for (String b : currLine.split("")) {
+				refSequence.put(tmpPos,b); 
+				tmpPos++;
+			}
+			currLine = br1.readLine();
+		}
+		br1.close();
+		ArrayList<Integer> availPos = new ArrayList<Integer>();
+		availPos.addAll(refSequence.keySet());
+		int refSeqLen = availPos.size(); 
+
+		// 2) int[varPos] and int[hap][varPos] = refOrAlt.
+		// Put all of the simulated haplotype information (from the ms simulation)
+		// program into into accessibl objects.
+		BufferedReader br2 = new BufferedReader(new FileReader(args[1])); 
+		currLine = br2.readLine();
+		int numHaps = Integer.parseInt(currLine.split(" ")[1]); 
+		for (int i = 0; i < 5; i++) currLine = br2.readLine();
+		int numVarPos = Integer.parseInt(currLine.split(" ")[1]);
+		currLine = br2.readLine();
+		String[] tmpVarPos = currLine.split(" "); 
+		ArrayList<Integer> allVarPos = new ArrayList<Integer>(); 
+		for (int p = 1; p < numVarPos + 1; p++) {
+			allVarPos.add((int) Math.floor(Double.parseDouble(tmpVarPos[p]) * refSeqLen));
+			// System.out.println(tmpVarPos.length + "\t" + p);			
+		}
+		currLine = br2.readLine();
+		double[][] simHapVC = new double[numHaps][numVarPos]; 
+		for (int h = 0; h < numHaps; h++) {
+			String[] tmpHap = currLine.split(""); 
+			for (int p = 0; p < numVarPos; p++) 
+				simHapVC[h][p] = Integer.parseInt(tmpHap[p]); 
+			currLine = br2.readLine();
+		}
+		br2.close();
+
+		// 3) HashMap<varPos,altAllele>. Simulate mutations on the reference.
+		HashMap<Integer,String> allAltAlleles = new HashMap<Integer,String>();
+		double rateIndels = Double.parseDouble(args[2]); 
+		double extendInsert = Double.parseDouble(args[3]); 
+		for (int p = 0; p < numVarPos; p++)  {
+			allAltAlleles.put(allVarPos.get(p),
+				simVariant(refSequence.get(allVarPos.get(p)),rateIndels, extendInsert));
+		}
+
+		// 4) HashMap<hap,ArrayList<base>>. 
+		// Add simulated mutations to finish the simulated haplotypes.
+		HashMap<Integer,ArrayList<String>> allSimHaps = new HashMap<Integer,ArrayList<String>>();
+		PrintWriter pw0 = new PrintWriter(args[4] + "/simhaps.mutations.txt"); 
+		for (int h = 0; h < numHaps; h++) {
+			allSimHaps.put(h, new ArrayList<String>()); 
+			for (int p = 0; p < refSeqLen; p++) {
+				if (allVarPos.indexOf(p)!=-1) {
+					int varPosIndex = allVarPos.indexOf(p);
+					if (simHapVC[h][varPosIndex]==1) {
+						allSimHaps.get(h).add(allAltAlleles.get(p));
+						int oneIndexed = p + 1;
+						pw0.append("Haplotype_" + h + "\t" + oneIndexed + "\t" + refSequence.get(p) +"\t");
+						if (allAltAlleles.get(p).isEmpty()) pw0.append("*\n");
+						else pw0.append(allAltAlleles.get(p) + "\n");
+					} else 
+						allSimHaps.get(h).add(refSequence.get(p)); 
+				} else allSimHaps.get(h).add(refSequence.get(p));
+			}
+		}
+		pw0.close();
+
+		// 5) Simulate the patients and give a random number of haplotypes.
+		int numPts = Integer.parseInt(args[5]);	
+		int ptHaps = Integer.parseInt(args[6]); 	// Between 3 and 15 individual viruses in each patient.
+		for (int p = 0; p < numPts; p++) {
+			PrintWriter pw = new PrintWriter(args[4] + "/p" + p + ".fa");
+			for (int h = 0; h < ptHaps; h++) {
+				int currHap = ThreadLocalRandom.current().nextInt(0, numHaps); // Randomly pick an index from the list of haplotypes.
+				pw.append(">Haplotype_" + currHap + " \n");
+				for (String s : allSimHaps.get(currHap)) pw.append(s);
+				pw.append("\n\n"); 
+			}
+			pw.close();
+		}
+	}
+
+	static String simVariant(String refBase, double rateIndels, double extendInsert) {
+		String[] bases = new String[] {"A","C","G","T"};
+		if (ThreadLocalRandom.current().nextDouble() < rateIndels) {
+			if (ThreadLocalRandom.current().nextDouble() > 0.5) {
+				return "";
+			} else { 
+				StringBuilder insertBases = new StringBuilder(); 
+				insertBases.append(refBase); 
+				insertBases.append(bases[ThreadLocalRandom.current().nextInt(0, 4)]); 
+				double extension = ThreadLocalRandom.current().nextDouble(); 
+				extension = ThreadLocalRandom.current().nextDouble(); 
+				while (extension < extendInsert) {
+					insertBases.append(bases[ThreadLocalRandom.current().nextInt(0, 4)]); 
+					extension = ThreadLocalRandom.current().nextDouble(); 
+				}
+				return insertBases.toString(); 
+				
+			}
+		} else {
+			bases = ArrayUtils.removeElement(bases, refBase);
+			// for (String b : bases) System.out.print(b);
+			// System.out.println();
+			return bases[ThreadLocalRandom.current().nextInt(0, 3)]; 
+		}
+	}
+}
