369124ba97884875ac8402d0c18cfed78428fb07
Lauren Mak <laurenmak93@gmail.com>
2018-08-31 11:03:29 -0600

    CHANGELOG: Updates to submitting jobs and reporting intermediate results.
    PoolAnalyzer: rh2 calculation updated from mu hh (v0.2) to hh mu (original). Re-added rmlow_and_normalize, with cutoff updated to rare_cutoff/(best_Haps.num_curr_H * num_pools). Increased frequency of reporting rh2 and p_new, and moved to after normalizing the distribution and removing very low-frequency haplotypes. Also added a very frequent reporter for delta, which seems to be where the problem is since AEM is not cutting off early enough when the frequencies have all gone to NaN. Added a way to eliminate rjMCMC from PHX altogether. Updated the fulfill function in CurrentHap to actually fill the ranks i.e.: add positive eigenvalues to the zeroed out rows in the SVM for both num_haps < and >= num_snps. No longer reports reconstructed haplotypes with a frequency of 0 in p.all.results. Will now only report haplotypes with greater than 0% interpool frequency in the p.all.results file. Print intrapool frequencies rounded to the third decimal place so it's easier to read. 
    
    Orig2Recons: Complete overhaul. Now only reports the concise values (num_haps, prop_error, inter_freq_diff etc.) and does so for all simulations in an experimental group. Will take the housing directory of all iterations corresponding to a pathname and print all of the concise values to a single text file instead of to STDOUT as previously. The verbose report has been entirely removed. 
    
    BAMFormatter: Corrected the way that reads were counted because the estimated number of individuals was wildly off previously, leading to mistaken estimates in frequency.
    
    FullSimulator: Now takes the num_pts x num_haps original haplotypes from ms and distributes them individually.
    
    Pipeline_Submitter_v0.3.pl: Has been updated from qsub to slurm. Will now run as many iterations for each combination of parameters as desired, submitting each iteration as a separate batch job. Does not include Orig2Recons.jar until we find a way to sleep the reporting till the last simulation for each experiment is completed. 
    
    Pipeline_Randsim: This version sets the number of segregating sites for ms, and sets  mu = 2.5 x 10^-6. Added filter for 'called' variants with an extremely low allele-frequency estimate, which seemed to be more likely to be false (see line 106). All paths have been updated for Theia. Have removed BQSR for now because ApplyBQSR is throwing errors where PrintReads did not (says that the lack of content in RecalTable0 is making it short) and the result is that the BQSR-ed BAM is empty. For some reason, sometimes GATK HC will output more than one alternate allele even if the maxAltAllele is set to 1. No idea why. ms updated to produce num_pts x num_haps original haplotypes to distribute individually instead of just num_haps and handing them out randomly. This seems like a more realistic approximation of neutral evolution.
    
    Added R scripts for evaluating PHX's performance in different dataset.
    
    Added all of the external jar dependencies.

--- Results_Zipper.pl
+++ Results_Zipper.pl
@@ -1,14 +1,17 @@
 #!/usr/bin/perl
 
-my @mutationRate = (0.00000125, 0.0000025, 0.000025);
-my @hapsPerPat = (5, 10, 100);
+
+my @hapsPerPat = (5, 10);
+my $replicates = 3; 
 my $zipNames; 
 
-for (my $m = 0; $m < scalar(@mutationRate); $m++) {
+for (my $m = 0; $m < scalar(@hapsPerPat); $m++) {
 	for (my $h = 0; $h < scalar(@hapsPerPat); $h++) {
-		system("zip $mutationRate[$m]\_$hapsPerPat[$h].zip runthrough_v0.2_$mutationRate[$m]\_$hapsPerPat[$h]/simhaps.c.txt runthrough_v0.2_$mutationRate[$m]\_$hapsPerPat[$h]/simhaps.mutations.txt runthrough_v0.2_$mutationRate[$m]\_$hapsPerPat[$h]/p.all.vcf runthrough_v0.2_$mutationRate[$m]\_$hapsPerPat[$h]/p.all.results runthrough_v0.2_$mutationRate[$m]\_$hapsPerPat[$h]/p*.fa");
-		$zipNames .= "$mutationRate[$m]\_$hapsPerPat[$h].zip ";
+		for (my $r = 0; $r < $replicates; $r++) {
+			system("zip $hapsPerPat[$m]\_$hapsPerPat[$h]\_$r.zip bmb_v0.3_$hapsPerPat[$m]\_$hapsPerPat[$h]\_$r/simhaps.vc.txt bmb_v0.3_$hapsPerPat[$m]\_$hapsPerPat[$h]\_$r/simhaps.mutations.txt bmb_v0.3_$hapsPerPat[$m]\_$hapsPerPat[$h]\_$r/p.all.vcf bmb_v0.3_$hapsPerPat[$m]\_$hapsPerPat[$h]\_$r/p.all.pos bmb_v0.3_$hapsPerPat[$m]\_$hapsPerPat[$h]\_$r/p.all.results bmb_v0.3_$hapsPerPat[$m]\_$hapsPerPat[$h]\_$r/p*.fa");
+			$zipNames .= "$hapsPerPat[$m]\_$hapsPerPat[$h]\_$r.zip ";
+		}
 	}
 }
 
-system("zip MultiTest_results.zip $zipNames");
+system("zip BMB_PHX_results.zip $zipNames");
--- src/MiscFunctions/FullSimulator.java
+++ src/MiscFunctions/FullSimulator.java
@@ -48,8 +48,8 @@ public class FullSimulator {
 		// program into into accessibl objects.
 		BufferedReader br2 = new BufferedReader(new FileReader(args[1])); 
 		currLine = br2.readLine();
-		int numHaps = Integer.parseInt(currLine.split(" ")[1]); 
-		for (int i = 0; i < 5; i++) currLine = br2.readLine();
+		int rawNumHaps = Integer.parseInt(currLine.split(" ")[1]); 
+		for (int i = 0; i < 6; i++) currLine = br2.readLine();
 		int numVarPos = Integer.parseInt(currLine.split(" ")[1]);
 		currLine = br2.readLine();
 		String[] tmpVarPos = currLine.split(" "); 
@@ -59,14 +59,26 @@ public class FullSimulator {
 			// System.out.println(tmpVarPos.length + "\t" + p);			
 		}
 		currLine = br2.readLine();
-		double[][] simHapVC = new double[numHaps][numVarPos]; 
-		for (int h = 0; h < numHaps; h++) {
-			String[] tmpHap = currLine.split(""); 
-			for (int p = 0; p < numVarPos; p++) 
-				simHapVC[h][p] = Integer.parseInt(tmpHap[p]); 
+		ArrayList<String> allOrigHaps = new ArrayList<String>();
+		ArrayList<Integer> allOrigCts = new ArrayList<Integer>();  
+		for (int h = 0; h < rawNumHaps; h++) {
+			if (allOrigHaps.contains(currLine)) {
+				int tmpCt =  allOrigCts.get(allOrigHaps.indexOf(currLine)) + 1;
+				allOrigCts.set(allOrigHaps.indexOf(currLine), tmpCt);
+			} else {
+				allOrigHaps.add(currLine);
+				allOrigCts.add(1);
+			}
 			currLine = br2.readLine();
 		}
 		br2.close();
+		int numHaps = allOrigHaps.size(); 
+		double[][] simHapVC = new double[numHaps][numVarPos]; 
+		for (int h = 0; h < numHaps; h++) {
+			String[] tmpHap = allOrigHaps.get(h).split(""); 
+			for (int p = 0; p < numVarPos; p++) 
+				simHapVC[h][p] = Integer.parseInt(tmpHap[p]); 
+		}
 
 		// 3) HashMap<varPos,altAllele>. Simulate mutations on the reference.
 		HashMap<Integer,String> allAltAlleles = new HashMap<Integer,String>();
@@ -101,14 +113,19 @@ public class FullSimulator {
 
 		// 5) Simulate the patients and give a random number of haplotypes.
 		int numPts = Integer.parseInt(args[5]);	
-		int ptHaps = Integer.parseInt(args[6]); 	// Between 3 and 15 individual viruses in each patient.
+		int ptHaps = Integer.parseInt(args[6]); 
 		for (int p = 0; p < numPts; p++) {
 			PrintWriter pw = new PrintWriter(args[4] + "/p" + p + ".fa");
 			for (int h = 0; h < ptHaps; h++) {
 				int currHap = ThreadLocalRandom.current().nextInt(0, numHaps); // Randomly pick an index from the list of haplotypes.
+				while (allOrigCts.get(currHap) == 0) {
+					currHap = ThreadLocalRandom.current().nextInt(0, numHaps);
+				}
 				pw.append(">Haplotype_" + currHap + " \n");
 				for (String s : allSimHaps.get(currHap)) pw.append(s);
 				pw.append("\n\n"); 
+				int newCt = allOrigCts.get(currHap) - 1; 
+				allOrigCts.set(currHap, newCt);
 			}
 			pw.close();
 		}
