98a6129ecb3055abea1ed520ca657f8fe5206d0c
Michael Li <minghao.li1@ucalgary.ca>
2019-07-02 12:27:57 -0600

    Format phx-quan-dev changes
    - Trimmed whitespace and replaced tabs with 4 spaces.
    - Marked leftovers

--- src/PoolHap/Main.java
+++ src/PoolHap/Main.java
@@ -11,9 +11,9 @@ import java.time.LocalDateTime;
 public class Main {
     // TODO: [ReconEP]:: refactor variable names to be more sensible (e.g. camelCase for Java).
     // TODO: [Question]:: confusion over the HapLASSO portion.
-	
-	public HashMap<String, Integer> name2index=new HashMap<String, Integer>();
-	public String[] names_array;
+
+    public HashMap<String, Integer> name2index = new HashMap<String, Integer>();
+    public String[] names_array;
 
     public static void main(String[] args) throws Exception {
         /*
@@ -37,21 +37,25 @@ public class Main {
 
         // Print start time.
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        System.out.println("PoolHapX "+gp.function+" Initiated: " + dtf.format(LocalDateTime.now()));
+        System.out.println(
+            "PoolHapX " + gp.function + " Initiated: " + dtf.format(LocalDateTime.now()));
 
         /*
          *  Local haplotype configuration.
          */
- //       HapConfig[] initial_local_haps = new HapConfig[num_pools]; // HapConfig array; 1 per pool
-        // by design, HapConfig contains haps of all pools.
-        
-        if(gp.function.equals("gc")) {
-        	PrintWriter in_list = new PrintWriter( // writer object for graph colored pools
-                    new FileWriter(new File(gp.inter_dir + prefix + "_p.in.list")));
+        // TODO: LEFTOVER ML 20190702
+        // HapConfig[] initial_local_haps = new HapConfig[num_pools]; // HapConfig array; 1 per pool
 
-                // Apply graph coloring for each pool. Note, initial "global" haplotypes are roughly linked
-                // by GC with gaps between VARIANTS (not regions).
-        	for (int p = 0; p < num_pools; p++) {
+        // By design, HapConfig contains haps of all pools.
+        if (gp.function.equals("gc")) {
+            PrintWriter in_list = new PrintWriter( // writer object for graph colored pools
+                new FileWriter(new File(gp.inter_dir + prefix + "_p.in.list")));
+
+            // Apply graph coloring for each pool. Note, initial "global" haplotypes are roughly
+            // linked by GC with gaps between VARIANTS (not regions).
+            for (int p = 0; p < num_pools; p++) {
+                // TODO: [Question]:: this "pool_in" variable not used anywhere.
+                // Previously added to initial_local_haps but now commented out. ML 20190702
                 GraphColoring pool_in = new GraphColoring(
                     gp.inter_dir + prefix + "_p" + p + ".vef",
                     gs_var_pos,
@@ -59,96 +63,105 @@ public class Main {
 
                 System.out.println("Graph colouring for pool " + p + " is finished.");
                 in_list.println(gp.inter_dir + prefix + "_p" + p + ".in"); // write GC pool output
- //               initial_local_haps[p] = pool_in.hapOut(); // add pool HapConfig to array
+
+                // TODO: LEFTOVER
+                // initial_local_haps[p] = pool_in.hapOut(); // add pool HapConfig to array
+
             }
 
             in_list.close();
-            System.out.println("\nGraph-Coloring Finished: " + dtf.format(LocalDateTime.now()) + "\n");
-        }
-        
-        else if(gp.function.equals("aem")) { // Note: aem includes DC and AEM
-        	// Apply divide and conquer across all pools.
+            System.out.println(
+                "\nGraph-Coloring Finished: " + dtf.format(LocalDateTime.now()) + "\n");
+
+        } else if (gp.function.equals("aem")) { // Note: aem includes DC and AEM
+            // Apply divide and conquer across all pools.
             DivideConquer dc_maker = new DivideConquer(
                 gs_var_pos,
                 gp.inter_dir + prefix + "_p.in.list",
                 parameter_file,
                 dc_out_file);
+
             System.out.println("DC Finished: " + dtf.format(LocalDateTime.now()) + "\n");
-            
+
             /*
              *  Global haplotype configuration.
              */
 
-            // If there are enough gaps to run local reconstruction...
-//            if (dc_maker.region_solve) {
-                // Resolve level 1 regions with AEM; regional LASSO if AEM fails.
-                HapConfig[] level_I_config = dc_maker.analyze_regions(
-                    dc_maker.regions_level_I,
-                    parameter_file,
-                    gp.inter_dir + prefix,
-                    1);
+            // TODO: LEFTOVER ML 20190702
+            // // If there are enough gaps to run local reconstruction...
+            // if (dc_maker.region_solve) {
 
-                System.out.println("Level 1 AEM Finished: "
-                    + dtf.format(LocalDateTime.now())
-                    + "\n");
+            // Resolve level 1 regions with AEM; regional LASSO if AEM fails.
+            HapConfig[] level_I_config = dc_maker.analyze_regions(
+                dc_maker.regions_level_I,
+                parameter_file,
+                gp.inter_dir + prefix,
+                1);
 
-                // Resolve level 2 regions with the same strategy as above.
-                HapConfig[] level_II_config = dc_maker.analyze_regions(
-                    dc_maker.regions_level_II,
-                    parameter_file,
-                    gp.inter_dir + prefix,
-                    2);
+            System.out.println("Level 1 AEM Finished: "
+                + dtf.format(LocalDateTime.now())
+                + "\n");
 
-                System.out.println("Level 2 AEM Finished: "
-                    + dtf.format(LocalDateTime.now())
-                    + "\n");
+            // Resolve level 2 regions with the same strategy as above.
+            HapConfig[] level_II_config = dc_maker.analyze_regions(
+                dc_maker.regions_level_II,
+                parameter_file,
+                gp.inter_dir + prefix,
+                2);
 
-                // Link regions by applying graph coloring across the level 1 and level 2 regional
-                // haplotype configurations.
-                GraphColoring region_linker = new GraphColoring(
-                    level_I_config,
-                    level_II_config,
-                    gs_var_pos,
-                    gp.fragments);
+            System.out.println("Level 2 AEM Finished: "
+                + dtf.format(LocalDateTime.now())
+                + "\n");
 
-                // Write final global haplotype configurations (inter pool) to output.
-                HapConfig final_global_haps; // final global haplotype configuration object
-                final_global_haps = region_linker.hapOut(); // HapConfig from GC-linked regions
-                final_global_haps.write_global_file_string( // write to output
-                    gp.out_dir + prefix + "_gc.inter_freq_vars.txt",
-                    false);
+            // Link regions by applying graph coloring across the level 1 and level 2 regional
+            // haplotype configurations.
+            GraphColoring region_linker = new GraphColoring(
+                level_I_config,
+                level_II_config,
+                gs_var_pos,
+                gp.fragments);
 
-                System.out.println("\nPost-AEM GC Finished: " + dtf.format(LocalDateTime.now()) + "\n");
+            // Write final global haplotype configurations (inter pool) to output.
+            HapConfig final_global_haps; // final global haplotype configuration object
+            final_global_haps = region_linker.hapOut(); // HapConfig from GC-linked regions
+            final_global_haps.write_global_file_string( // write to output
+                gp.out_dir + prefix + "_gc.inter_freq_vars.txt",
+                false);
 
-            // Else, not enough gaps to run regional construction; existing haplotype configurations
-            // deemed "global" enough already.
-//            } 
-//            else {
-//                final_global_haps = new HapConfig(gp.inter_dir + prefix + "_p", gs_var_pos, num_pools);
-//                // final_global_haps = new HapConfig(initial_local_haps);
-//                final_global_haps.write_global_file_string( // write to output
-//                        gp.out_dir + prefix + "_gc.inter_freq_vars.txt",
-//                        false);
-//            }
+            System.out.println("\nPost-AEM GC Finished: " + dtf.format(LocalDateTime.now()) + "\n");
 
-        }
-        
+            // TODO: LEFTOVER ML 20190702
+            // // Else, not enough gaps to run regional construction; existing haplotype
+            // // configurations deemed "global" enough already.
+            // } else {
+            //     final_global_haps = new HapConfig(
+            //         gp.inter_dir + prefix + "_p",
+            //         gs_var_pos,
+            //         num_pools);
 
-        /*
-         *  Final intra pool haplotype configuration via LASSO selection.
-         *  TODO: [Question]:: are these global intra pool haplotypes?
-         */
-        
-        else if(gp.function.equals("lasso")) {
-        	HapConfig final_global_haps=new  HapConfig(gp.out_dir + prefix + "_gc.inter_freq_vars.txt",
-           		 null);
-        	SiteInPoolFreqAnno siteInPoolFreqAnno = new SiteInPoolFreqAnno(gs_var_pos);
-        	final_global_haps.inpool_site_freqs=siteInPoolFreqAnno.inpool_freqs;
-        	final_global_haps.num_pools=num_pools;
-        	final_global_haps.pool_IDs=siteInPoolFreqAnno.pool_IDs;
-        	final_global_haps.in_pool_haps_freq=
-        			new double[final_global_haps.num_global_hap][final_global_haps.num_pools];
-        	HapConfig[] final_local_haps = new HapConfig[num_pools]; // final intra pool HapConfigs
+            //     // final_global_haps = new HapConfig(initial_local_haps);
+            //     final_global_haps.write_global_file_string( // write to output
+            //             gp.out_dir + prefix + "_gc.inter_freq_vars.txt",
+            //             false);
+            // }
+
+        } else if(gp.function.equals("lasso")) {
+            /*
+             *  Final intra pool haplotype configuration via LASSO selection.
+             *  TODO: [Question]:: are these global intra pool haplotypes?
+             */
+            HapConfig final_global_haps = new HapConfig(
+                gp.out_dir + prefix + "_gc.inter_freq_vars.txt",
+                null);
+
+            SiteInPoolFreqAnno siteInPoolFreqAnno = new SiteInPoolFreqAnno(gs_var_pos);
+            final_global_haps.inpool_site_freqs = siteInPoolFreqAnno.inpool_freqs;
+            final_global_haps.num_pools = num_pools;
+            final_global_haps.pool_IDs = siteInPoolFreqAnno.pool_IDs;
+            final_global_haps.in_pool_haps_freq =
+                    new double[final_global_haps.num_global_hap][final_global_haps.num_pools];
+
+            HapConfig[] final_local_haps = new HapConfig[num_pools]; // final intra pool HapConfigs
 
            // Apply LASSO for each pool from final global haplotypes to estimate frequencies of each
            // in each pool.
@@ -205,7 +218,5 @@ public class Main {
                "string",
                false);
         }
-        
-
     }
 }
