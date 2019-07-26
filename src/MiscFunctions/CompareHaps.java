c181adbac417c676940a6897b379133dcc9a202c
Jennifer He <jingni.he1@ucalgary.ca>
2019-07-26 16:45:17 -0600

    changes to CompareHaps

--- src/MiscFunctions/CompareHaps.java
+++ src/MiscFunctions/CompareHaps.java
@@ -236,10 +236,10 @@ public class CompareHaps {
     }
     
     public static void main(String[] args) throws IOException, InterruptedException {
-    	String project_name= args[0];
-    	double quasi_cutoff= Double.parseDouble(args[1]); // "0.01"
-    	String gs_dir= args[2];
-    	String output_dir= args[3]; 
+    	String project_name= args[0];  //for example 0_1
+    	double quasi_cutoff= Double.parseDouble(args[1]); // cutoff
+    	String gs_dir= args[2]; //gold_standard dir
+    	String output_dir= args[3]; //out_put dir
         String ori_inter_file=gs_dir+project_name+"_haps.inter_freq_vars.txt";
         String ori_intra_file=gs_dir+project_name+"_haps.intra_freq.txt";
         String recon_inter_file=output_dir+project_name+".inter_freq_vars.txt";
