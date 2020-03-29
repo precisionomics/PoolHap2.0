71608e96dbf39114488de6df724675d17bee6b6d
Chen Cao <quanlonglab@gmail.com>
2020-03-28 18:45:30 -0600

    remove

--- Perfect_Data_Related/PD_Step1.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-#SBATCH -J PHX_Submission	# The job name
-
-import sys
-import subprocess
-
-for s in range(0, int(sys.argv[1])):
-	subprocess.call(['sbatch', '-J', 'RunPD_' + str(s), '-o', 'RunPD_' + str(s), 'PHX_run.py', str(s), str(sys.argv[2])])
